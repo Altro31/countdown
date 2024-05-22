c9082534a993207b400fc83a644929f3a2d67fbb
Altro <119718837+Altro31@users.noreply.github.com>
2024-05-22 09:21:43 -0400

    feat: move target hour to 9 o'clock

--- src/utils/hooks/useRest.ts
+++ src/utils/hooks/useRest.ts
@@ -1,6 +1,6 @@
 import {Time} from "@/utils/interfaces/Time";
 
-const target = {hours: 23, minutes: 59, seconds: 59}
+const target = {hours: 21, minutes: 59, seconds: 59}
 
 export function useRest() {
     const now = new Date()
