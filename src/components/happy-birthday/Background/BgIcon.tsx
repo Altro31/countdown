9f18a7f8064d21e22160e62d0ad3552824f697bd
The Altro <albe020531@outlook.com>
2024-05-23 00:08:16 -0400

    feat: last changes

--- src/components/happy-birthday/Background/BgIcon.tsx
+++ src/components/happy-birthday/Background/BgIcon.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 interface Props {
     children: React.ReactNode
+    className?: string
 }
 
-export function BgIcon({children}: Props) {
+export function BgIcon({children, className}: Props) {
 
     return (
-        <>
+        <div className={className}>
             {children}
-        </>
+        </div>
     )
 }
