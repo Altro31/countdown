73ad679798a4f45dd172ca342b0c26c203c90360
The Altro <albe020531@outlook.com>
2024-05-23 00:03:46 -0400

    feat: last changes

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,6 +5,7 @@ import {DoubleDotsSeparator} from "@/components/DoubleDotsSeparator";
 import type {Time as TimeType} from "@/utils/interfaces/Time";
 import {Time} from "./Time";
 import {Messages} from "@/components/Messages";
+import {useRouter} from "next/navigation";
 
 interface Props {
     initialTime: TimeType
@@ -13,6 +14,10 @@ interface Props {
 export function Timer({initialTime}: Props) {
     const {time} = useTimer()
     const renderTime = time ?? initialTime
+    const router = useRouter()
+    if (renderTime.hours === 0 && renderTime.minutes === 0 && renderTime.seconds === 0) {
+        router.push('/happy-birthday')
+    }
     return (
         <div className="flex flex-col text-9xl text-slate-200 md:flex-row md:gap-2 md:text-9xl select-none">
             <Time value={renderTime.hours} type="hours"/>
